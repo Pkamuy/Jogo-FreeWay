a1cdcfe95002e39831dce04210b388079b8483f7
Pedro Anderson Duarte Cardoso <pedroanderson.cardoso@gmail.com>
2022-03-02 13:40:27 -0300

    Adicionando codigo do Jogo da Vaca

---
+++ images.js
@@ -0,0 +1,26 @@
+// imagens e sons do jogo
+
+//imagens
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons
+let somColisao;
+let somPontos;
+let somTrilha;
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");  
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+  
+  somTrilha = loadSound("sons/trilha.mp3");
+  somPontos = loadSound("sons/pontos.wav");
+  somColisao = loadSound("sons/colidiu.mp3");
+}
